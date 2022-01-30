eba06c3f7c459c1390885d68235bd9df3bf0fb0e
nabezokodaikon <nabezoko.daikon@gmail.com>
2022-01-30 19:12:10 +0900

    Use relative path

--- denops/@ddu-sources/mr.ts
+++ denops/@ddu-sources/mr.ts
@@ -1,10 +1,11 @@
 import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v0.1.0/types.ts";
-import { Denops } from "https://deno.land/x/ddu_vim@v0.1.0/deps.ts";
+import { Denops, fn } from "https://deno.land/x/ddu_vim@v0.1.0/deps.ts";
 import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.1.0/file.ts#^";
 import {
   ensureArray,
   isString,
 } from "https://deno.land/x/unknownutil@v1.1.4/mod.ts";
+import { relative } from "https://deno.land/std@0.122.0/path/mod.ts#^";
 
 const kinds = ["mrr", "mrw", "mru"];
 
@@ -21,11 +22,12 @@ export class Source extends BaseSource<Params> {
   }): ReadableStream<Item<ActionData>[]> {
     return new ReadableStream({
       async start(controller) {
+        const dir = await fn.getcwd(args.denops) as string;
         const idx = kinds.indexOf(args.sourceParams.kind);
         const result = await args.denops.call(`mr#${kinds.at(idx)}#list`);
         ensureArray(result, isString);
         controller.enqueue(result.map((p) => ({
-          word: p,
+          word: relative(dir, p),
           action: {
             path: p,
           },
